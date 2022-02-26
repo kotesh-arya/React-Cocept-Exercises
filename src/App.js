6738d8e6c1660be88fa62191cbdbe9ed2b5591f2
kotesh_Mudila <69259490+kotesh-arya@users.noreply.github.com>
2022-02-26 15:56:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -21,29 +21,45 @@ export default function App() {
   // const sampleFunction = () => {
   //   console.log("Hello baby");
   //   return 7;
+  // ------------------------Dev-Ed-simplified-example----------------------
+  // const [state, setState] = useState({ count: 1, theme: "black" });
+  // let count = state.count;
+  // let theme = state.theme;
+  // function incrementValue() {
+  //   setState((prevState) => {
+  //     return { ...prevState, count: prevState.count + 1 };
+  //   });
+  // }
+  // const decrementValue = () => {
+  //   // setState((prevCount) => prevCount.count + 1);
+  // };
+  // ------------------------Dev-Ed-simplified-example----------------------
 
-  const [state, setState] = useState({ count: 1, theme: "black" });
-  let count = state.count;
-  let theme = state.theme;
-  function incrementValue() {
-    setState((prevState) => {
-      return { ...prevState, count: prevState.count + 1 };
-    });
-  }
-  const decrementValue = () => {
-    // setState((prevCount) => prevCount.count + 1);
-  };
+  // ------------------------useState-React-Docs(Ex-3-Checkbox)----------------------
+
+  // In this example, the liked state variable holds a boolean. When you click the input, setLiked updates the liked state variable with whether the browser checkbox input is checked. The liked variable is used to render the text below the checkbox.
+
+  //  const [liked,setLiked ] = useState(false);
+  //  const onChangeHandler = (e) =>{
+  //    setLiked(e.target.checked)
+  //  }
+  // ------------------------useState-React-Docs(Ex-3-Checkbox)----------------------
+
   return (
     <div className="App">
-      {/* <h2 onClick={headclick}>Now the count after a click is {count}!</h2>
-      <input onClick={boxChecked} type="checkbox" checked={like} />{" "}
-      <p>{like ? "likes this" : "Don't like this"} This</p> */}
-
-      <button onClick={decrementValue}>-</button>
+      {/* ------------------------Dev-Ed-simplified-example---------------------- */}
+      {/* <button onClick={decrementValue}>-</button>
       <span>
         {count} {theme}
       </span>
-      <button onClick={incrementValue}>+</button>
+      <button onClick={incrementValue}>+</button> */}
+      {/* ------------------------Dev-Ed-simplified-example---------------------- */}
+      {/* ------------------------useState-React-Docs(Ex-3-Checkbox)---------------------- */}
+      {/* 
+     <input onChange={onChangeHandler} type="checkbox" checked ={liked}></input>
+     <p> {liked ? "likes this" :"Don't like this"}</p> */}
+
+      {/* ------------------------useState-React-Docs---------------------- */}
     </div>
   );
 }
